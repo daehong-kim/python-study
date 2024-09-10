6df46478761e1b3ebe97ad795375c4b566a3d18e
daehong-kim <dh9184@gmail.com>
2024-09-10 19:07:21 +0900

    1강 공부내용 정리

---
+++ 1강/index.py
@@ -0,0 +1,65 @@
+
+# print
+# print("hello")
+# print("world")
+
+# 변수는 영어, _, 숫자를 사용해서 선언 단 숫자는 첫글자가 될수 없다, 
+# 예) 영어_숫자 , 영어 대소문자 구분, 사용자 입력
+# a=10
+# print(a)
+
+# input
+# input("이름을 입력해 주세요")
+# print("이름은", name, "입니다")
+
+# 자료형은 데이터의 종류, 숫자형 문자열 군집자료형, 숫자형에는 정수와 실수로 구분
+# 정수형에는 int형이 있다(일반숫자) < 실수형에는 float형이 있다 (소수점 포함) 
+# 특징 1 연산가능, 2 숫자함수 사용
+
+# a=9
+# b=5
+# print(type(a+b))
+# print(type(a-b))
+# print(type(a/b)) # 결과값이 float임
+# print(type(a*b))
+# print(type(a%b))
+# print(type(a//b))
+# print(type(a**b))
+# python 특징 : type 추론에 따라 데이터형 판단으로 속도가 느리다, 유저는 편하다
+
+# round, range, pow  for i in range(5)
+# print(round(3.1465, 2))
+# print(pow(2,4))
+# for i in range(5) : 
+#     print("아무거나")
+
+
+
+
+
+# 비교 연산자 : bool
+# print (10>15)
+# print (16<15)
+# # 작거나 같다, 크거나 같다, 좌우동일하다, 동일하지 않다
+# print (16<=15)
+# print (14>=15)
+# print (16==15)
+# print (15!=15)
+
+
+
+
+
+
+# 논리연산자 and or, 로그인 되어있고 vip 유저라면 ~~ 해라 (도메인)
+# a=15
+# print(a<10 and a>12) # t, 
+
+
+# 문자열 (문자와 문자열 동일하게 처리) str, 연산불가, 문자열에 맞는 함수(split, ) 특수조건 (문자 + 문자, 문자*숫자)
+
+# print ("a"+"a")
+# print ("5"+"5")
+# print ("a"*5)
+
+
